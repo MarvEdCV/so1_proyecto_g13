b10d3fa278dbcd37d69816a467463f6c03f31352
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-10-26 22:51:44 -0600

    update

--- Go Consumer Kafka/main.go
+++ Go Consumer Kafka/main.go
@@ -13,13 +13,13 @@ func main() {
 	conn, _ := kafka.DialLeader(context.Background(), "tcp", "kafka-cluster-kafka-bootstrap:9092", "input-kafka", 0)
 	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
 
-	batch := conn.ReadBatch(1e3, 1e6)
-	bytes := make([]byte, 1e3)
+	message, _ := conn.ReadMessage(1e6)
+	/*bytes := make([]byte, 1e3)
 	for {
 		_, err := batch.Read(bytes)
 		if err != nil {
 			break
-		}
-		fmt.Println(string(bytes))
-	}
+		}*/
+	fmt.Println(message.Value)
+	//}
 }
