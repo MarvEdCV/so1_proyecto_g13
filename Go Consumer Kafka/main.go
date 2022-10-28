0e45a85d00b96c683ca4e5266a3d9ddc7c42100c
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-10-27 19:10:01 -0600

    Update consumer

--- Go Consumer Kafka/main.go
+++ Go Consumer Kafka/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/segmentio/kafka-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
 var MONGO_URL = "mongodb://cosmosdb-mongo-sopes1:FQZmTPpDpwOjpqkAUdkdLrT8qnCZktOjbm3Fi6R2lddZUSWTtHre1yQGm7NEVTE5VV4zU9thxpzPACDb8hKlKA==@cosmosdb-mongo-sopes1.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosdb-mongo-sopes1@"
@@ -28,7 +32,7 @@ func main() {
 	conf := kafka.ReaderConfig{
 		Brokers:     []string{"kafka-cluster-kafka-bootstrap:9092"},
 		Topic:       "input-kafka",
-		GroupID:     "matches-group",
+		GroupID:     "g1",
 		StartOffset: kafka.LastOffset,
 	}
 	reader := kafka.NewReader(conf)
@@ -40,29 +44,28 @@ func main() {
 			continue
 		}
 		fmt.Println("El mensaje es: ", string(message.Value))
-	}
-	/*var doc interface{}
-	errr := bson.UnmarshalExtJSON([]byte(message.Value), true, &doc)
-	if errr != nil {
-		log.Fatal(errr)
-	}
-	//CONEXION A LA BASE DE DATOS E INSERCION DE DATOS
-	client, err := mongo.NewClient(options.Client().ApplyURI(MONGO_URL))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
+		var doc interface{}
+		errr := bson.UnmarshalExtJSON([]byte(message.Value), true, &doc)
+		if errr != nil {
+			log.Fatal(errr)
+		}
+		//CONEXION A LA BASE DE DATOS E INSERCION DE DATOS
+		client, err := mongo.NewClient(options.Client().ApplyURI(MONGO_URL))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Disconnect(ctx)
 
-	collection := client.Database("UsactarMongoDB").Collection("data")
-	res, insertErr := collection.InsertOne(ctx, doc)
-	if insertErr != nil {
-		log.Fatal(insertErr)
+		collection := client.Database("UsactarMongoDB").Collection("data")
+		res, insertErr := collection.InsertOne(ctx, doc)
+		if insertErr != nil {
+			log.Fatal(insertErr)
+		}
+		fmt.Println(res)
 	}
-	fmt.Println(res)
-	*/
 }
