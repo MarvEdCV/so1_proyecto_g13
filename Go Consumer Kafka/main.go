8162bb8e09562943b9050ae215e5278f93dcc694
Eduardo Catalán <3026773130104@ingenieria.usac.edu.gt>
2022-11-01 01:10:39 -0600

    Se guarda en redis por medio de grcp

--- Go Consumer Kafka/main.go
+++ Go Consumer Kafka/main.go
@@ -7,28 +7,47 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strings"
 	"time"
 )
 
 var MONGO_URL = "mongodb://cosmosdb-mongo-sopes1:FQZmTPpDpwOjpqkAUdkdLrT8qnCZktOjbm3Fi6R2lddZUSWTtHre1yQGm7NEVTE5VV4zU9thxpzPACDb8hKlKA==@cosmosdb-mongo-sopes1.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosdb-mongo-sopes1@"
 
+func saveGrcp(dato string) {
+	url := "http://34.66.132.45:8083/client-grcp"
+	method := "POST"
+
+	payload := strings.NewReader(dato)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
+
 func main() {
 	log.Println("Estoy en el consumer :)")
-	/*
-		conn, _ := kafka.DialLeader(context.Background(), "tcp", "kafka-cluster-kafka-bootstrap:9092", "input-kafka", 0)
-		conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
-
-		message, _ := conn.ReadMessage(1e6)
-		/*bytes := make([]byte, 1e3)
-		for {
-			_, err := batch.Read(bytes)
-			if err != nil {
-				break
-			}
-		fmt.Println(string(message.Value))
-		//}
-	*/
 	conf := kafka.ReaderConfig{
 		Brokers:     []string{"kafka-cluster-kafka-bootstrap:9092"},
 		Topic:       "input-kafka",
@@ -44,6 +63,7 @@ func main() {
 			continue
 		}
 		fmt.Println("El mensaje es: ", string(message.Value))
+		saveGrcp(string(message.Value))
 		var doc interface{}
 		errr := bson.UnmarshalExtJSON([]byte(message.Value), true, &doc)
 		if errr != nil {
